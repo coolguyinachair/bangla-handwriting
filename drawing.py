f74b608f1a67e23a8096ffb7679661bd24570b16
Arnab De <arnabdilip@gmail.com>
2024-05-12 07:54:11 +0530

    Add bangla.py for bangla specific actions

--- drawing.py
+++ drawing.py
@@ -6,21 +6,23 @@ import numpy as np
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
 
-
-alphabet = [
-    '\x00', ' ', '!', '"', '#', "'", '(', ')', ',', '-', '.',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
-    '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-    'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'Y',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-    'y', 'z'
-]
+from bangla import alphabet, alphabet_ord, alpha_to_num, num_to_alpha
 
 
-alphabet_ord = list(map(ord, alphabet))
-alpha_to_num = defaultdict(int, list(map(reversed, enumerate(alphabet))))
-num_to_alpha = dict(enumerate(alphabet_ord))
+# alphabet = [
+#     '\x00', ' ', '!', '"', '#', "'", '(', ')', ',', '-', '.',
+#     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
+#     '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+#     'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'Y',
+#     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+#     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+#     'y', 'z'
+# ]
+#
+#
+# alphabet_ord = list(map(ord, alphabet))
+# alpha_to_num = defaultdict(int, list(map(reversed, enumerate(alphabet))))
+# num_to_alpha = dict(enumerate(alphabet_ord))
 
 MAX_STROKE_LEN = 1200
 MAX_CHAR_LEN = 75
